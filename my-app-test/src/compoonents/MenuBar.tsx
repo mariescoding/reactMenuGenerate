a5fbf35bd73b1ff937dd62bd4fa40f64311cae6e
Marie Muramatsu <muramatsumarie@gmail.com>
2023-08-16 17:25:50 +0900

    add box to typograhy & change icon button component

--- my-app-test/src/compoonents/MenuBar.tsx
+++ my-app-test/src/compoonents/MenuBar.tsx
@@ -2,21 +2,27 @@ import Box from "@mui/material/Box";
 import AppBar from "@mui/material/AppBar";
 import Typography from "@mui/material/Typography";
 import Toolbar from "@mui/material/Toolbar";
+import IconButton from "@mui/material/IconButton";
+import HomeOutlinedIcon from "@mui/material/IconButton";
+import MenuBookOutlinedIcon from "@mui/material/IconButton";
 
 export const MenuBar = () => {
   return (
     <Box sx={{ flexGrow: 1 }}>
       <AppBar position="static">
         <Toolbar>
-          <Typography
-            variant="h5"
-            sx={{ fontWeight: "bold", ml: 2, flexGrow: 1 }}
-          >
-            Seeds
-          </Typography>
+          <Box sx={{ ml: 2, flexGrow: 1 }}>
+            <Typography variant="h5" sx={{ fontWeight: "bold" }}>
+              Seeds
+            </Typography>
+          </Box>
 
-          {/* <Iconbutton iconname={<HomeOutlinedIcon />} />
-            <Iconbutton iconname={<MenuBookOutlinedIcon />} /> */}
+          <IconButton size="medium" edge="end" color="inherit" sx={{ mr: 2 }}>
+            <HomeOutlinedIcon />
+          </IconButton>
+          <IconButton size="medium" edge="end" color="inherit" sx={{ mr: 2 }}>
+            <MenuBookOutlinedIcon />
+          </IconButton>
         </Toolbar>
       </AppBar>
     </Box>
