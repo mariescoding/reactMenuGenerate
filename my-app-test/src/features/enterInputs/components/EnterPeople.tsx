a4bf6ffd027223a9c41670ae6986ac1d5967b461
Marie Muramatsu <muramatsumarie@gmail.com>
2023-08-16 15:42:58 +0900

    add people button values

--- my-app-test/src/features/enterInputs/components/EnterPeople.tsx
+++ my-app-test/src/features/enterInputs/components/EnterPeople.tsx
@@ -1,13 +1,27 @@
 import Grid from "@mui/material/Grid";
 import Typography from "@mui/material/Typography";
-import { PersonButton } from "./PersonButton";
+//import { PersonButton } from "./PersonButton";
+import { people } from "/Users/mariemuramatsu/Personal/React/my-app-test/src/demo_data/people.js";
+import Chip from "@mui/material/Chip";
 
 export const EnterPeople = () => {
+  const peopleList = people.map((person) => {
+    return (
+      <Grid item xs={6}>
+        <Chip
+          label={person.name}
+          variant="outlined"
+          sx={{ width: "90%" }}
+        ></Chip>
+      </Grid>
+    );
+  });
   return (
     <Grid item md={6}>
       <Typography variant="h6">Who's eating?</Typography>
-      <Grid container spacing={2} sx={{ m: 3 }}>
-        <PersonButton personName={""} />
+      <Grid container spacing={2} sx={{ p: 3, mt: 1 }}>
+        {/* <PersonButton /> */}
+        {peopleList}
       </Grid>
     </Grid>
   );
--- my-app-test/src/features/enterInputs/components/PersonButton.tsx
+++ my-app-test/src/features/enterInputs/components/PersonButton.tsx
@@ -1,14 +1,10 @@
 import Chip from "@mui/material/Chip";
 import Grid from "@mui/material/Grid";
 
-type Props = {
-  personName: String;
-};
-
-export const PersonButton: React.FC<Props> = ({ personName }) => {
-  return (
-    <Grid item>
-      <Chip label={personName} variant="outlined"></Chip>
-    </Grid>
-  );
-};
+// export const PersonButton: React.FC = () => {
+//   return (
+//     <Grid item  xs={6}>
+//       <Chip label={""} variant="outlined"></Chip>
+//     </Grid>
+//   );
+// };
