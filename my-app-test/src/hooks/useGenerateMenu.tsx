27ee1d1d1b5f1c8adf5d6151b79af17e88f6e3a4
Marie Muramatsu <muramatsumarie@gmail.com>
2023-08-18 11:46:58 +0900

    add changing boolean code

--- my-app-test/src/hooks/useGenerateMenu.tsx
+++ my-app-test/src/hooks/useGenerateMenu.tsx
@@ -2,8 +2,23 @@ import React from "react";
 import { useState } from "react";
 
 export const useGenerateMenu = () => {
-  const [isPersonClicked, setisPersonClicked] = useState([]);
-  const [isMenuClicked, setisMenuClicked] = useState([]);
+  const [ingIsClicked, setIngIsClicked] = useState(Array(5).fill(false));
+  const [personIsClicked, setPersonIsClicked] = useState(Array(4).fill(false));
 
-  return { isPersonClicked, isMenuClicked };
+  function ingChipClick(id: number) {
+    console.log("function called");
+
+    const copyIngClicked = ingIsClicked.slice();
+    copyIngClicked[id] = !copyIngClicked[id];
+    setIngIsClicked(copyIngClicked);
+    console.log(copyIngClicked, ingIsClicked);
+  }
+
+  function personChipClick(id: number) {
+    const copyPersonClicked = personIsClicked.slice();
+    copyPersonClicked[id] = !copyPersonClicked[id];
+    setPersonIsClicked(copyPersonClicked);
+  }
+
+  return { ingChipClick, personChipClick, ingIsClicked, personIsClicked };
 };
