100fcfd6c7ee0279133f7428db6a4c77684cd890
Marie Muramatsu <muramatsumarie@gmail.com>
2023-08-21 11:47:20 +0900

    add component for disaplying menu

--- my-app-test/src/pages/home.tsx
+++ my-app-test/src/pages/home.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import Box from "@mui/material/Box";
 import Container from "@mui/material/Container";
 import { Title } from "../compoonents/Title";
-// import { EnterInputs } from "../features/enterInputs/EnterInputs";
+import { DisplayTodayMenu } from "../features/displayTodayMenu/displayTodayMenu";
 
 export const Home = () => {
   return (
@@ -12,6 +12,11 @@ export const Home = () => {
           <Box sx={{ m: 7, fontWeight: "bold" }}>
             <Title label="Today's Menu" />
           </Box>
+          <DisplayTodayMenu
+            imageUrl=""
+            foodName="chicken rice"
+            ingName="pork"
+          />
         </Container>
       </Box>
     </>
