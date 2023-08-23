f84fda414042ab047e40843b4398629542b266f8
Marie Muramatsu <muramatsumarie@gmail.com>
2023-08-23 16:17:41 +0900

    change data flow

--- my-app-test/src/features/displayTodayMenu/components/MenuDetail.tsx
+++ my-app-test/src/features/displayTodayMenu/components/MenuDetail.tsx
@@ -9,10 +9,11 @@ import { MenuRating } from "../../../compoonents/MenuRating";
 
 type Props = {
   ingName: String;
-  avgRate: number;
+  rating: number[];
 };
 
-export const MenuDetail: React.FC<Props> = ({ ingName, avgRate }) => {
+export const MenuDetail: React.FC<Props> = ({ ingName, rating }) => {
+  const avgRate = rating.reduce((a, b) => a + b) / rating.length;
   return (
     <Grid container spacing={2} sx={{ p: 3, mt: 1, mb: 1 }}>
       <Grid item md={5}>
--- my-app-test/src/features/displayTodayMenu/displayTodayMenu.tsx
+++ my-app-test/src/features/displayTodayMenu/displayTodayMenu.tsx
@@ -7,32 +7,33 @@ import React from "react";
 import { MenuDetail } from "./components/MenuDetail";
 
 type Props = {
-  imageUrl: string;
-  foodName: String;
-  ingName: String;
-  avgRate: number;
+  todayMenu: {
+    id: number;
+    name: string;
+    ingredient: string;
+    image: string;
+    rating: number[];
+  };
 };
 
-export const DisplayTodayMenu: React.FC<Props> = ({
-  imageUrl,
-  foodName,
-  ingName,
-  avgRate,
-}) => {
+export const DisplayTodayMenu: React.FC<Props> = ({ todayMenu }) => {
   return (
     <Paper elevation={3} sx={{ height: "380px;" }}>
       <Grid container spacing={2} sx={{ p: 3 }}>
         <Grid item md={6}>
           <Box sx={{ width: "90%", height: "80%", mt: 3, ml: 1, mb: 3 }}>
-            <img alt="foodImage" src={imageUrl}></img>
+            <img alt="foodImage" src={todayMenu.image}></img>
           </Box>
         </Grid>
         <Grid item md={6}>
           <Box sx={{ pb: 3 }}>
-            <Typography variant="h5">{foodName}</Typography>
+            <Typography variant="h5">{todayMenu.name}</Typography>
           </Box>
           <Divider />
-          <MenuDetail ingName={ingName} avgRate={avgRate} />
+          <MenuDetail
+            ingName={todayMenu.ingredient}
+            rating={todayMenu.rating}
+          />
         </Grid>
       </Grid>
     </Paper>
